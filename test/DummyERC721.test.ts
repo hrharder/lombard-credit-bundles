d95f665f053401fc657aeb2873967ff867ad540f
Henry Harder <henry@periscopetrading.com>
2020-07-16 13:31:24 -0400

    update tests

--- test/DummyERC721.test.ts
+++ test/DummyERC721.test.ts
@@ -4,18 +4,18 @@ import { expect } from "chai";
 import "mocha";
 
 const DummyNFT = contract.fromArtifact("DummyERC721");
-describe("Mint", async () => {
-  const [owner, otherAccount] = accounts;
+describe("DummyERC721", async () => {
+    const [owner] = accounts;
 
-  let dummyNFT: any = undefined;
-  beforeEach(async function() {
-    dummyNFT = await DummyNFT.new({ from: owner });
-  });
+    let dummyNFT: any = undefined;
+    beforeEach(async function() {
+        dummyNFT = await DummyNFT.new({ from: owner });
+    });
 
-  it("should be able to mint an NFT with any ID", async () => {
-    // mint token to self
-    await dummyNFT.mint(3, { from: owner });
+    it("should be able to mint an NFT with any ID", async () => {
+        // mint token to self
+        await dummyNFT.mint(3, { from: owner });
 
-    expect(await dummyNFT.ownerOf(3)).to.eql(owner);
-  });
+        expect(await dummyNFT.ownerOf(3)).to.eql(owner);
+    });
 });
