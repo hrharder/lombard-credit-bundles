c1d374d51623dda8e0f4d08104dbd92241a313af
Liam Kovatch <liam@periscopetrading.com>
2020-07-16 20:33:23 -0700

    LoanBundleFactory

---
+++ contracts/LoanBundleFactory.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.5.0;
+
+import "./LoanBundle.sol";
+
+contract LoanBundleFactory {
+    function deployLoanBundleToken(
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimals,
+        address payable[] memory _loanArray,
+        uint256 _supply
+    ) public returns (address) {
+        LoanBundle newLoanBundle = (
+            new LoanBundle(_name, _symbol, _decimals, _loanArray, _supply)
+        );
+        return address(newLoanBundle);
+    }
+}
